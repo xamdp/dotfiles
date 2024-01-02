4826fd31dc5b12f2b3179f3d40d390a9764490e7
xamdp <diegoysmaeldocot@gmail.com>
2024-01-02 14:16:46 +0800

    add harpoon2 config

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,13 +1,18 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+local harpoon = require("harpoon")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
 
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
-vim.keymap.set("n", "<C-j>", function() ui.nav_file(5) end)
--- vim.keymap.set("n", "<C-k>", function() ui.nav_file(6) end) // already have a keybind for ctrl + k which is signature helps from lsp
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-t>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-m>", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
 
