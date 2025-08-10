2ea5dea7242692271d0df3bf15d431feb156afab
xamdp <64017084+xamdp@users.noreply.github.com>
2025-08-10 20:29:05 +0900

    Add autocmds and a new plugin
    Added autocmds for the lsp diagnostics and its mappings.
    And a new plugin lspkind.

---
+++ nvim/nvim/lsp/emmet_ls.lua
@@ -0,0 +1,14 @@
+return {
+	cmd = {
+		"emmet-ls", "--stdio"
+	},
+
+	filetypes = {
+		"astro", "css", "eruby", "html", "htmlangular", "htmldjango", "javascriptreact", "less",
+		"pug", "sass", "scss", "svelte", "templ", "typescriptreact", "vue",
+	},
+
+	root_markers = {
+		".git"
+	}
+}
--- nvim/nvim/lua/plugins/essentials.lua
+++ nvim/nvim/lua/plugins/essentials.lua
@@ -1,7 +1,7 @@
 return {
 	'mbbill/undotree',
-	{ 
-		'nvim-tree/nvim-web-devicons', 
-		opts = {} 
+	{
+		'nvim-tree/nvim-web-devicons',
+		opts = {}
 	},
 }
