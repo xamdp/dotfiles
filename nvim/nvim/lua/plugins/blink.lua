a5608a7db7c2e47cef31570a38ef1a9213ea9a7d
xamdp <64017084+xamdp@users.noreply.github.com>
2025-08-28 09:36:33 +0900

    Add mappings for blink
    Added a mapping where I can cycle through items using Shift and
    Shift+Tab.

--- nvim/nvim/lua/plugins/blink.lua
+++ nvim/nvim/lua/plugins/blink.lua
@@ -7,6 +7,8 @@ return {
 		keymap = {
 			preset = "default",
 			["<CR>"] = { "accept", "fallback" },
+			["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
 			["<C><leader>"] = { "show" },
 		},
 		appearance = {
