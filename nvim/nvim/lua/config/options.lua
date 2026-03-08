57913b8fb55edb6028097ef2cdab5837c5780121
xamdp <64017084+xamdp@users.noreply.github.com>
2026-03-08 11:27:30 +0900

    fix whitespace

--- nvim/nvim/lua/config/options.lua
+++ nvim/nvim/lua/config/options.lua
@@ -1,25 +1,27 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.g.have_nerd_font = false
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 vim.opt.showmode = false
-vim.schedule(function() vim.opt.clipboard = 'unnamedplus' end)
+vim.schedule(function()
+	vim.opt.clipboard = "unnamedplus"
+end)
 vim.opt.breakindent = true
 vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 300
 vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.list = false
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.inccommand = 'split'
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.inccommand = "split"
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 vim.opt.winborder = "solid" -- https://neovim.io/doc/user/options.html#'winborder'cmd.Ex)
@@ -53,5 +55,11 @@ vim.diagnostic.config({
 			[vim.diagnostic.severity.ERROR] = "ErrorMsg",
 			[vim.diagnostic.severity.WARN] = "WarningMsg",
 		},
+	},
+})
+
+vim.filetype.add({
+	extension = {
+		["http"] = "http",
 	},
 })
