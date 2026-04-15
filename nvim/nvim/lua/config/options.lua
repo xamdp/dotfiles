9f205f33396bbfa4806e14879ffd7cd131aa52db
xamdp <64017084+xamdp@users.noreply.github.com>
2026-04-15 20:11:23 +0900

    add folds

--- nvim/nvim/lua/config/options.lua
+++ nvim/nvim/lua/config/options.lua
@@ -27,6 +27,13 @@ vim.opt.scrolloff = 10
 vim.opt.winborder = "solid" -- https://neovim.io/doc/user/options.html#'winborder'cmd.Ex)
 vim.opt.termguicolors = true
 vim.o.termguicolors = true
+
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.o.foldcolumn = "1"
+vim.o.foldenable = true
+vim.o.foldlevel = 99
+vim.o.foldnestmax = 3
 -- vim.o.clipboard = 'unnamedplus'
 
 -- vim.g.loaded_netrw = 1
